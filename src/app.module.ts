1a829ca457d51b55667215ed2106a8692822d934
arman <arman.u.alam@gmail.com>
2025-04-23 23:21:23 +0600

    chore: add winston for logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,17 +11,17 @@ import { UsersModule } from './users/users.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: process.env.POSTGRES_HOST || 'localhost',
-    //   port: parseInt(process.env.POSTGRES_PORT!) || 5432,
-    //   username: process.env.POSTGRES_USER || 'postgres',
-    //   password: process.env.POSTGRES_PASSWORD || 'postgres',
-    //   database: process.env.POSTGRES_DB || 'nestjs',
-    //   entities: [User],
-    //   synchronize: true, //set to false in production
-    // }),
-    // UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT!) || 5432,
+      username: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      database: process.env.POSTGRES_DB || 'nestjs',
+      entities: [User],
+      synchronize: true, //set to false in production
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
